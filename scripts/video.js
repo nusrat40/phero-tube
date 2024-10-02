3917e59e3accc5b49c8ef8da0ac8214112b14a02
Nusrat Jahan <140106780+nusrat40@users.noreply.github.com>
2024-10-02 21:25:44 +0600

    added the api fetching part

---
+++ scripts/video.js
@@ -0,0 +1,29 @@
+//fetch,load and show categories on html
+
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+
+
+// "category_id": "1001",
+//  "category": "Music"
+
+const displayCategories = (categories) => {
+ const categoryContainer =document.getElementById("categories");
+
+  categories.forEach((item) => {
+    console.log(item);
+    //create a button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+
+    //add button to category container
+    categoryContainer.append(button);
+  });
+};
+
+loadCategories();
